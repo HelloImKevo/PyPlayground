6fb66d7f8dd6252a7c57552970e36956cb70f466
Kevin Schanz <kschanz@imobile3.com>
2020-04-17 17:12:19 -0400

    Delete an old file and address PEP inspections in UI test

--- src/userinterface/ui_test_1.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-
-root = Tk()
-the_label = Label(root, text="This is too easy")
-the_label.pack()
-
-root.mainloop()
--- src/userinterface/ui_test_7.py
+++ src/userinterface/ui_test_7.py
@@ -1,29 +1,30 @@
+import tkinter
 from tkinter import *
 
 root = Tk()
 
 
-def leftClick(event):
-    print("Left")
+def left_click(event: tkinter.Event):
+    print("Left Mouse Button (LMB)", type(event))
 
 
-def middleClick(event):
-    print("Middle")
+def middle_click(event: tkinter.Event):
+    print("Middle Mouse Button (MMB)", type(event))
 
 
-def rightClick(event):
-    print("Right")
+def right_click(event: tkinter.Event):
+    print("Right Mouse Button (RMB)", type(event))
 
 
-root.title("Button Click Test")
+root.title("Mouse Button Click Test")
 
 frame = Frame(root, width=300, height=250)
 
 # Button numbers are dependent on the OS.
 # On Mac, Button #2 is a right click!
-frame.bind("<Button-1>", leftClick)
-frame.bind("<Button-2>", rightClick)
-frame.bind("<Button-3>", middleClick)
+frame.bind("<Button-1>", left_click)
+frame.bind("<Button-2>", right_click)
+frame.bind("<Button-3>", middle_click)
 
 frame.pack()
 
